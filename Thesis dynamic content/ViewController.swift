02482f83d6721716d8b7939535934655c0ed43d5
erokha <erokhin@meetyapp.ru>
2022-04-03 21:10:04 +0300

    main: [init] created project

---
+++ Thesis dynamic content/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Thesis dynamic content
+//
+//  Created by erokha on 4/3/22.
+//
+
+import UIKit
+
+final class InitialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
